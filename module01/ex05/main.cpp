7a552f6faed1d8dc04657c0c5fa6a8938ef4b7cb
Diane Vallienne <dvallien@z4r9p5.42lyon.fr>
2022-08-07 12:16:39 +0200

    name

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -1,34 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/06 14:19:58 by dvallien          #+#    #+#             */
-/*   Updated: 2022/08/07 11:05:55 by dvallien         ###   ########.fr       */
+/*   Created: 2022/08/06 14:19:41 by dvallien          #+#    #+#             */
+/*   Updated: 2022/08/07 11:22:20 by dvallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_H
-# define HARL_H
-# include <iostream>
-# include <string>
+#include "Harl.hpp"
 
-class Harl{
-	
-	public:
-		Harl( void );
-		~Harl( void );
-		void	complain( std::string level );
-	
-	private:
-		void			_debug( void );
-		void			_info( void );
-		void			_warning( void );
-		void			_error( void );
-		void			(Harl::*_ptrF[4])( void );
-		std::string		_lvl[4];
-};
 
-#endif
+int main( int ac, char **av )
+{
+	Harl		harl;
+	std::string level;
+	
+	if (ac == 2)
+	{
+		level = av[1];
+		harl.complain(level);
+	}
+	else
+		std::cout << "Error : wrong number of arguments" << std::endl;
+	return (0);
+}
