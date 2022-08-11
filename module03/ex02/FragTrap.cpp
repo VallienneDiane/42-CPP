d7638d8b444948beaeaff5efb5dc3d517dd7c90a
Diane Vallienne <dvallien@z3r3p3.42lyon.fr>
2022-08-11 16:59:54 +0200

    almost

---
+++ module03/ex02/FragTrap.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 16:44:44 by dvallien          #+#    #+#             */
+/*   Updated: 2022/08/11 16:52:29 by dvallien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
+#include "FragTrap.hpp"
+
+/**********************************************/
+/*			     CONSTRUCTORS                 */
+/* ********************************************/
+
+FragTrap::FragTrap( void )
+{
+	this->_name = "unknow";
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+	std::cout << ORANGE << "FragTrap default constructor called : " << this->_name << std::endl;
+	return;
+}
+
+FragTrap::FragTrap( std::string name ) : ClapTrap(name)
+{
+	this->_name = name;
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+	std::cout << ORANGE << "FragTrap name constructor called : " << name << std::endl;
+	return;
+}
+
+FragTrap::FragTrap(const FragTrap &src)
+{
+	this->setName(src._name);
+	this->setHitP(src._hitPoints);
+	this->setEnergyP(src._energyPoints);
+	this->setAttackP(src._attackDamage);
+	std::cout << ORANGE << "FragTrap copy constructor called : " << this->_name << std::endl;
+	*this = src;
+}
+
+FragTrap & FragTrap::operator=(const FragTrap &src)
+{
+	std::cout << ORANGE << "FragTrap copy assignment operator called : " << this->_name << std::endl;
+	this->_name = src._name;
+	this->_hitPoints = src._hitPoints;
+	this->_energyPoints = src._energyPoints;
+	this->_attackDamage = src._attackDamage;
+	return (*this);
+}
+
+/**********************************************/
+/*			        FUNCTIONS                 */
+/* ********************************************/
+
+void FragTrap::highFivesGuys(void) // Cette fonction membre affiche une demande de high fives sur la sortie standard.
+{
+	std::cout << BLUE << "High fives guys !" << std::endl;
+}
+
+/**********************************************/
+/*			     DESTRUCTOR                   */
+/* ********************************************/
+
+FragTrap::~FragTrap( void )
+{
+	std::cout << WHITE << "FragTrap destructor called : " << this->_name << std::endl;
+	return;
+}
---
+++ module03/ex02/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 14:18:40 by dvallien          #+#    #+#             */
+/*   Updated: 2022/08/11 16:49:24 by dvallien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int main(void)
+{
+	FragTrap	unamed;
+	FragTrap	badGuy("Gargamel");
+	ScavTrap	grumpyGuy("SchtroumpfGrognon");
+	ScavTrap	cat("Azrael");
+	ClapTrap	target("GrandSchtroumpf");
+	ClapTrap	veryBadGuy(badGuy);
+	
+	std::cout << std::endl;
+	veryBadGuy.attack(unamed.getName());
+	unamed.takeDamage(20);
+	unamed.setName("Joe");
+	unamed.beRepaired(10);
+	std::cout << std::endl;
+	cat.attack(unamed.getName());
+	unamed.takeDamage(20);
+	unamed.beRepaired(10);
+	std::cout << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		veryBadGuy.attack(grumpyGuy.getName());
+		grumpyGuy.takeDamage(20);
+	}
+	badGuy.attack(grumpyGuy.getName());
+	grumpyGuy.takeDamage(20);
+	grumpyGuy.beRepaired(10);
+	std::cout << std::endl;
+	grumpyGuy.attack(cat.getName());
+	cat.guardGate();
+	std::cout << std::endl;
+	target.attack(badGuy.getName());
+	std::cout << std::endl;
+	badGuy.attack(target.getName());
+	target.takeDamage(20);
+	target.beRepaired(30);
+	std::cout << std::endl;
+	return (0);
+}
