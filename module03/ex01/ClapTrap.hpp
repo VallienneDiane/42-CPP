fc235d8aae8efd2e5de46197b835dab645c43b83
Diane Vallienne <dvallien@z3r3p3.42lyon.fr>
2022-08-11 16:42:12 +0200

    ok

---
+++ module03/ex01/ClapTrap.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 14:19:28 by dvallien          #+#    #+#             */
+/*   Updated: 2022/08/11 16:41:44 by dvallien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+#include <string>
+
+# define RED "\x1B[31m"
+# define GREEN "\x1B[32m"
+# define YELLOW "\x1B[33m"
+# define ORANGE "\x1B[34m"
+# define PURPLE "\x1B[35m"
+# define BLUE "\x1B[36m"
+# define GREY "\x1B[30m"
+# define WHITE "\x1B[37m"
+
+class ClapTrap{
+
+	public:
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &src);
+		ClapTrap & operator=(const ClapTrap &src);
+		~ClapTrap(void);
+		void		setName(std::string name);
+		void		setHitP(int hitPoints);
+		void		setEnergyP(int energyPoints);
+		void		setAttackP(int attackDamage);
+		std::string	getName(void) const;
+		int			getHitP(void) const;
+		int			getEnergyP(void) const;
+		int			getAttackD(void) const;
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		
+	protected:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+};
+
+#endif
