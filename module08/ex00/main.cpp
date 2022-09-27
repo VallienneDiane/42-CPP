6a04a6df66716d6e813a2186d178e1e336dd34dd
Diane Vallienne <dvallien@z4r2p1.42lyon.fr>
2022-09-27 13:35:26 +0200

    ok

--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 10:43:26 by dvallien          #+#    #+#             */
-/*   Updated: 2022/09/14 13:58:11 by dvallien         ###   ########.fr       */
+/*   Updated: 2022/09/27 13:30:42 by dvallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,36 @@
 
 int main(void)
 {
-	std::vector<int> vInt(3);
-	std::vector<int> vInt2(10, 95); // 10 values initialize at 2
-	std::list<int> lInt;
-	std::vector<char> vChar(4);
-	std::list<int> lChar(4);
+	std::vector<int> vectorInt(3);
+	std::vector<int> vectorInt2(10, 42); // 10 values initialized at 42
+	std::list<int> listInt;
 	
-	vInt[0] = 14;
-	vInt[1] = 28;
-	vInt[2] = 35;
-	lInt.push_back(14);
-	lInt.push_back(15);
-	vChar[0] = 'h';
-	vChar[1] = 'e';
-	vChar[2] = 'l';
-	vChar[3] = 'l';
-	lChar.push_back('b');
-	lChar.push_back('e');
-	lChar.push_back('e');
-	lChar.push_back('s');
+	std::vector<char> vectorChar(4);
+	std::list<int> listChar(4);
+	
+	vectorInt[0] = 14;
+	vectorInt[1] = 28;
+	vectorInt[2] = 35;
+	
+	listInt.push_back(14);
+	listInt.push_back(15);
+
+	vectorChar[0] = 'h';
+	vectorChar[1] = 'e';
+	vectorChar[2] = 'l';
+	vectorChar[3] = 'l';
+	
+	listChar.push_back('b');
+	listChar.push_back('e');
+	listChar.push_back('e');
+	listChar.push_back('s');
+	
 	std::cout << "--------------- Find int : vector ---------------" << std::endl;
 	try
 	{
-		easyfind(vInt, 35);
-		easyfind(vInt2, 95);
-		easyfind(vInt, 15);
+		easyfind(vectorInt, 35);
+		easyfind(vectorInt2, 95);
+		easyfind(vectorInt, 15);
 	}
 	catch(const std::exception& e)
 	{
@@ -48,8 +53,8 @@ int main(void)
 	std::cout << "--------------- Find char : vector ---------------" << std::endl;
 	try
 	{
-		easyfind(vChar, 'e');
-		easyfind(vChar, 'f');
+		easyfind(vectorChar, 'e');
+		easyfind(vectorChar, 'f');
 	}
 	catch(const std::exception& e)
 	{
@@ -59,8 +64,8 @@ int main(void)
 	std::cout << "--------------- Find int : list ---------------" << std::endl;
 	try
 	{
-		easyfind(lInt, 14);
-		easyfind(lInt, 16);
+		easyfind(listInt, 14);
+		easyfind(listInt, 16);
 	}
 	catch(const std::exception& e)
 	{
@@ -70,8 +75,8 @@ int main(void)
 	std::cout << "--------------- Find char : list ---------------" << std::endl;
 	try
 	{
-		easyfind(lChar, 'b');
-		easyfind(lChar, 'o');
+		easyfind(listChar, 'b');
+		easyfind(listChar, 'o');
 	}
 	catch(const std::exception& e)
 	{
