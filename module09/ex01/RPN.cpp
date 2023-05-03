1d21881cbfa307acf49a78bb530934ee6712f472
VallienneDiane <diane.vallienne@outlook.com>
2023-05-03 11:44:00 +0200

    in pro

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -12,19 +12,19 @@
 
 #include "RPN.hpp"
 
-int checkNumbers(int number) {
-    if(number < 0 || number > 10) {
-        std::cout << "Error: wrong number format" << std::endl;
-        return(1);
-    }
-    return (0);
-}
+// int checkNumbers(int number) {
+//     if(number < 0 || number > 10) {
+//         std::cout << "Error: wrong number format" << std::endl;
+//         return(1);
+//     }
+//     return (0);
+// }
 
 int checkElementType(std::string polishExpr) {
 
     for(size_t i = 0; i < polishExpr.length(); i++) {
         if(!isdigit(polishExpr[i]) && polishExpr[i] != '*' && polishExpr[i] != '+' && polishExpr[i] != '-' && polishExpr[i] != '/' && polishExpr[i] != ' ') {
-            std::cout << "Error: wrong expression format : polishExpr[i] = " << polishExpr[i] << std::endl;
+            std::cout << "Error: wrong expression format" << std::endl;
             return (1);
         }
     }
@@ -45,17 +45,50 @@ int checkNbElements(std::string polishExpr) {
     }
     countNumbers--;
     if(countNumbers != countOperators) {
-        std::cout << "Error: too much operators or numbers" << std::endl;
+        std::cout << "Error: invalid expression2" << std::endl;
         return (1);
     }
     return(0);
+}
+
+float doTheMath(std::stack<int> stack, char ope) {
+    float result = 0;
+    float newNb1 = stack.top();
+    stack.pop();
+    float newNb2 = stack.top();
+    stack.pop();
+    switch(ope) 
+    {
+        case '+': 
+            result = newNb2 + newNb1;
+            stack.push(result);
+            break;
+        case '-':
+            result = newNb2 - newNb1;
+            stack.push(result);
+            break;
+        case '*':
+            result = newNb2 * newNb1;
+            stack.push(result);
+            break;
+        case '/':
+            if(newNb1 == 0) {
+                std::cout << "Error: impossible division" << std::endl;
+                break;
+            }
+            result = newNb2 / newNb1;
+            stack.push(result);
+            break;
+        default : 
+            break;
+    }
+    return (result);
 }
 
 int checkData(std::string polishExpr) 
 {
     std::stringstream ss(polishExpr);
     std::stack<int> stack;
-    int result = 0;
 
     if(polishExpr.size() == 0) {
         std::cout << "Error: empty expression" << std::endl;
@@ -67,43 +100,25 @@ int checkData(std::string polishExpr)
         return (1);
 
     for(size_t i = 0; i < polishExpr.length(); i++) {
-        //remove spaces !!!
+        if(polishExpr.length() < 3) {
+            std::cout << "Error: invalid expression1" << std::endl;
+            return (1);
+        }
+        while(polishExpr[i] == ' ') {
+            i++;
+        }
         if(isdigit(polishExpr[i])) {
             int number = polishExpr[i] - 48;
-            if(checkNumbers(number) == 0) {
-                stack.push(number);
-            }
+            stack.push(number);
         }
         else {
             char ope = polishExpr[i];
-            if(stack.size() >= 2) {
-                int newNb1 = stack.top();
-                std::cout << ope << " nb1 " << newNb1 << std::endl;
-                stack.pop();
-                int newNb2 = stack.top();
-                std::cout << ope << " nb2 " << newNb2 << std::endl;
-                stack.pop();
-                switch(ope) 
-                {
-                    case '+': 
-                        result = newNb2 + newNb1;
-                        std::cout << " result = " << result << std::endl;
-                        stack.push(result);
-                        break;
-                    case '-':
-                        result = newNb2 - newNb1;
-                        std::cout << " result = " << result << std::endl;
-                        stack.push(result);
-                        break;
-                    default : 
-                        break;
-                }
-            }
-            else {
-                std::cout << "Error: invalid input" << std::endl;
-                return (1);
+            if(stack.size() > 1) {
+                float result = doTheMath(stack, ope);
+                
             }
         }          
     }
+    std::cout << result << std::endl;
     return (0);
 }
