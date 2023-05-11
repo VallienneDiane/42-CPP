de3b6b5cddb237b57fd9a0bead4769f45db00485
VallienneDiane <diane.vallienne@outlook.com>
2023-05-11 18:51:25 +0200

    clean

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -64,28 +64,28 @@ int checkDate(std::string date)
     std::vector<std::string> splitDate;
 
     if(date.size() != 11 || checkCharactersDate(date) == false) {
-        std::cout << ORANGE << "Error: invalid date => \"" << date << "\"" << std::endl;
+        std::cout << "Error: invalid date => \"" << date << "\"" << std::endl;
         return (1);
     }
     splitDate = splitLine(date, '-');
-    if(splitDate[0].size() > 4 || splitDate[1].size() > 2 || splitDate[2].size() > 3) {
-        std::cout << ORANGE << "Error: invalid date => \"" << date << "\"" << std::endl;
+    if(splitDate[0].size() > 4 || splitDate[1].size() > 2 || splitDate[2].size() > 3 || splitDate[2][2] != ' ') {
+        std::cout << "Error: invalid date => \"" << date << "\"" << std::endl;
         return (1);
     }
     int year = atoi(splitDate[0].c_str());    
     int month = atoi(splitDate[1].c_str());
     int day = atoi(splitDate[2].c_str());
     if((day < 1 || day > 31) || (year < 2009) || (month < 1 || month > 12)) {
-        std::cout << ORANGE << "Error: invalid date => \"" << date << "\"" << std::endl;
+        std::cout << "Error: invalid date => \"" << date << "\"" << std::endl;
         return (1);
     }
     else if((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) {
-        std::cout << ORANGE << "Error: invalid date => \"" << date << "\"" << std::endl;
+        std::cout << "Error: invalid date => \"" << date << "\"" << std::endl;
         return (1);
     }
     else if((month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) && day > 29) 
     || (month == 2 && ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0)) && day > 28)) {
-        std::cout << ORANGE << "Error: invalid date, not a bissextile year => \"" << date << "\"" << std::endl;
+        std::cout << "Error: invalid date, not a bissextile year => \"" << date << "\"" << std::endl;
         return (1);
     }
     return (0);                
@@ -96,12 +96,20 @@ int checkValue(std::string value)
     size_t count = 0;
 
     for(size_t i = 0; i < value.size(); i++) {
-        if(value[i] == ' ' && i != 0) {
-            std::cout << PURPLE << "Error: invalid number => " << value << std::endl;  
+        if(i == 0 && value[i] != ' ') {
+            std::cout << "Error: missing space at the beginning" << std::endl;  
             return (1);
         }
-        if(!isdigit(value[i]) && value[i] != '.' && value[i] != '-' && value[i] != ' ') {
-            std::cout << PURPLE << "Error: not a number => " << value[i] << std::endl;  
+        else if(i != 0 && value[i] == ' ') {
+            std::cout << "Error: too many spaces" << std::endl;  
+            return (1);
+        }
+        else if(i != 0 && (value[i] == '-')) {
+            std::cout << "Error: not a positive number => " << value << std::endl;
+            return (1);
+        }
+        else if(i != 0 && (!isdigit(value[i]) && value[i] != '.')) {
+            std::cout << "Error: not a number =>" << value << std::endl;  
             return (1);
         }
         if(value[i] == '.') {
@@ -109,20 +117,15 @@ int checkValue(std::string value)
         }
     }
     if(count > 1) {
-        std::cout << PURPLE << "Error: not a float number => " << value << std::endl;
+        std::cout << "Error: not a float number => " << value << std::endl;
         return(1);
     }
-    float valuef = std::atof(value.c_str());
-    if(valuef < 0) {
-        std::cout << PURPLE << "Error: not a positive number => " << valuef << std::endl;
+    double valuef = std::atof(value.c_str());
+    if (valuef > 1000) {
+        std::cout << "Error: too large number => " << valuef << std::endl;
         return (1);
     }
-    else if (valuef > 1000) {
-        std::cout << PURPLE << "Error: too large number => " << valuef << std::endl;
-        return (1);
-    }
-    return (0);
-                    
+    return (0);                
 }
 
 int checkInput(char *inputFile, std::vector <std::pair<std::string, float> > &data)
@@ -133,6 +136,7 @@ int checkInput(char *inputFile, std::vector <std::pair<std::string, float> > &da
     std::string value;
     std::string date;
     float valuef;
+    int pipeNb = 0;
 
     if(myinput.is_open()) {
         std::getline(myinput, line);
@@ -142,9 +146,14 @@ int checkInput(char *inputFile, std::vector <std::pair<std::string, float> > &da
         }
         while(std::getline(myinput, line))
         {
+            for(size_t i = 0; i < line.size(); i++) {
+                if(line[i] == '|')
+                    pipeNb++;
+            }
             result = splitLine(line, '|');
-            if(result.size() != 2) {
-                std::cout << BLUE << "Error: invalid input => \"" << line << "\" " << std::endl;
+            if(result.size() != 2 || pipeNb != 1 || result[1].size() < 2) {
+                pipeNb = 0;
+                std::cout << "Error: invalid input => \"" << line << "\" " << std::endl;
             }
             else {
                 date = result[0];;
@@ -158,15 +167,15 @@ int checkInput(char *inputFile, std::vector <std::pair<std::string, float> > &da
                         date.erase(std::remove_if(date.begin(), date.end(), isspace), date.end());
                         valuef = (atof(value.c_str()));
                         if(strcmp((*it).first.c_str(), date.c_str()) == 0) {                         
-                            std::cout << WHITE << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
+                            std::cout << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
                             break;
                         }
                         else if((((it+1) != end) && (strcmp((*(it+1)).first.c_str(), date.c_str()) > 0)) && (strcmp((*it).first.c_str(), date.c_str()) < 0)) {
-                            std::cout << WHITE << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
+                            std::cout << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
                             break;
                         }
                         else if(((it+1) == end)) {
-                            std::cout << WHITE << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
+                            std::cout << date << " => " << value << " = " << (valuef * ((*it).second)) << std::endl;
                             break;
                         }
                     }
